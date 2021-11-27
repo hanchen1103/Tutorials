58c953152a96a2d00823c524dc19b1fc7e586169
hanchen1103 <3457580866@qq.com>
2021-11-27 22:18:50 +0800

    prototype

--- src/main/java/com/demo/newproject/config/DistributedComponentConfig.java
+++ src/main/java/com/demo/newproject/config/DistributedComponentConfig.java
@@ -11,13 +11,13 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-@Scope(value = "prototype")
 public class DistributedComponentConfig {
 
     @Autowired
     JedisAdapter jedisAdapter;
 
     @Bean
+    @Scope(value = "prototype")
     public ZKDistributedLock buildLock() {
         ZKDistributedLock zkDistributedLock = new ZKDistributedLock("/zookeeper", "127.0.0.1", 10000);
         zkDistributedLock.setLockValue("index");
@@ -25,6 +25,7 @@ public class DistributedComponentConfig {
     }
 
     @Bean
+    @Scope(value = "prototype")
     public RSDistributedLimit buildLimit() {
         return new RSDistributedLimit.JedisBuilder<>(jedisAdapter.getJedisConnection()).
                 maxSize(100).secondToken(5).wasteTicket(10).request("queuelist").create();
